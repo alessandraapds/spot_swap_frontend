2b4afc47324f8c40bdb7f20d641f2155d8425281
Alessandra Silva <alessandra.apds@gmail.com>
2023-04-01 16:13:21 +0200

    routes, navbar added

---
+++ src/views/NavBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            SpotSwap
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+              <a class="nav-link" href="#">
+                Offers
+              </a>
+              <a class="nav-link" href="#">
+                Bookings
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <Outlet />
+    </div>
+  );
+};
+
+export default Navbar;
