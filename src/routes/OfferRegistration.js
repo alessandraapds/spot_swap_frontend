b6e9fd25670f0f4cc7161f83a0491acc77cdc926
Alessandra Silva <alessandra.apds@gmail.com>
2023-04-03 21:33:19 +0200

    retrieving real data

--- src/routes/OfferRegistration.js
+++ src/routes/OfferRegistration.js
@@ -3,24 +3,124 @@ import React from "react";
 const OfferRegistration = () => {
   return (
     <div class="container">
-      <div>Register your offer</div>
+      <h2>Register your offer</h2>
       <form>
-        <div>
-          Offer Name:
+        {/* Offer name */}
+        <div class="form-group">
+          <label for="offerName">Name</label>
           <input
-            name="name"
-            id="name"
-            placeholder="Give a name to your parking spot"
+            type="text"
+            class="form-control"
+            id="offerName"
+            aria-describedby="emailHelp"
+            placeholder="Give your offer a name"
+            required
           />
         </div>
-        <div>
-          Address:
+
+        {/* Address */}
+        <div class="form-group">
+          <label for="address">Address</label>
           <input
-            name="name"
-            id="name"
-            placeholder="Give a name to your parking spot"
+            type="text"
+            class="form-control"
+            id="address"
+            placeholder="Tell us the address"
+            required
           />
         </div>
+
+        {/* -- Offer size */}
+        <div class="form-group">
+          <label for="offerSize">Offer size</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="inlineRadioOptions"
+            id="offerSize"
+            value="option1"
+            required
+          />
+          <label class="form-check-label" for="inlineRadio1">
+            Small
+          </label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="inlineRadioOptions"
+            id="inlineRadio2"
+            value="option2"
+          />
+          <label class="form-check-label" for="inlineRadio2">
+            Medium
+          </label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="inlineRadioOptions"
+            id="inlineRadio3"
+            value="option3"
+          />
+          <label class="form-check-label" for="inlineRadio3">
+            Big
+          </label>
+        </div>
+
+        {/* Price */}
+        <div class="form-group">
+          <label for="price">Price</label>
+          <input
+            type="number"
+            class="form-control"
+            id="price"
+            placeholder="Only numbers"
+            required
+          />
+        </div>
+
+        {/* Available from */}
+        <div class="form-group">
+          <label for="availableFrom">Available from</label>
+          <input
+            type="datetime-local"
+            class="form-control"
+            id="availableFrom"
+            placeholder="Tell us the address"
+            required
+          />
+        </div>
+
+        {/* Available until */}
+        <div class="form-group">
+          <label for="address">Available until</label>
+          <input
+            type="datetime-local"
+            class="form-control"
+            id="address"
+            placeholder="Tell us the address"
+            required
+          />
+        </div>
+
+        {/* Picture */}
+        <div class="form-group">
+          <label for="exampleFormControlFile1">Choose a picture: </label>
+          <input
+            type="file"
+            class="form-control-file"
+            id="exampleFormControlFile1"
+          />
+        </div>
+
+        <button type="submit" class="btn btn-primary">
+          List my offer
+        </button>
       </form>
     </div>
   );
--- src/routes/Offers.js
+++ src/routes/Offers.js
@@ -1,38 +1,11 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
 
 const Offers = () => {
-  const offersArray = [
-    {
-      _id: "642683024454d8e663574237",
-      offerName: "Christina garage",
-      latitude: 51.51,
-      longitude: 7.46,
-      street: "Eberstrasse 26",
-      city: "Dortmund",
-      postalCode: 44145,
-      startAvailableDate: "2023-05-05T14:30:00.000Z",
-      endAvailableDate: "2023-05-07T21:30:00.000Z",
-      offerSize: "small",
-      pricePerHour: 40,
-      isAvailable: true,
-    },
-    {
-      _id: "642683024454d8e663574239",
-      offerName: "Available parking",
-      latitude: 51.51,
-      longitude: 7.46,
-      street: "Burgweg 16",
-      city: "Dortmund",
-      postalCode: 44145,
-      startAvailableDate: "2023-05-05T18:00:00.000Z",
-      endAvailableDate: "2023-05-10T20:45:00.000Z",
-      offerSize: "medium",
-      pricePerHour: 60,
-      isAvailable: false,
-    },
-  ];
+  const url = "http://localhost:8001/offers";
+  const { data, isLoading, error } = useFetch(url);
 
-  return offersArray.map((offer) => {
+  return data.map((offer) => {
     return (
       <div class="container">
         <div class="row">
