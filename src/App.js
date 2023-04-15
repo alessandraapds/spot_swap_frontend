439ed2034d389adb994063af05182f7cd48af204
Luis Rojas <luisignacio1210@gmail.com>
2023-04-15 18:00:04 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
           <Route path="/payment/:id" element={<Payment />} />
           <Route path="/bookings/:id" element={<Bookings />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          
         </Route>
       </Routes>
     </>
